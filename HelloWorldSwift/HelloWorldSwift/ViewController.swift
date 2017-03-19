438c275189d502bac38f32d8fbc26c4bdcb0a9c8
Peng <1498284491@qq.com>
2017-03-19 12:48:25 +0800

    add a firsr swift project

---
+++ HelloWorldSwift/HelloWorldSwift/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HelloWorldSwift
+//
+//  Created by 云 Love on 2017/3/14.
+//  Copyright © 2017年 云 Love. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let mystring = "title"
+        print(mystring)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
