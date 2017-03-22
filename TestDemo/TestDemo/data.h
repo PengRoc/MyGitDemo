bc1de48ce9587cc7192c9e68d324f962dc755480
Peng <1498284491@qq.com>
2017-03-22 14:42:54 +0800

    add a model to project and a button a lab on main

---
+++ TestDemo/TestDemo/data.h
@@ -0,0 +1,13 @@
+//
+//  data.h
+//  TestDemo
+//
+//  Created by 云 Love on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface data : NSObject
+
+@end
---
+++ TestDemo/TestDemo/data.m
@@ -0,0 +1,13 @@
+//
+//  data.m
+//  TestDemo
+//
+//  Created by 云 Love on 2017/3/22.
+//  Copyright © 2017年 xfunsun. All rights reserved.
+//
+
+#import "data.h"
+
+@implementation data
+
+@end
